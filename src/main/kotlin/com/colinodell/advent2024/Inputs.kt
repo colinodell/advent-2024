702d2924d7c07f621566aff017fe7a580fadd6ad
Colin O'Dell <codell@seatgeek.com>
2024-12-01 08:58:47 -0500

    Lint

--- src/main/kotlin/com/colinodell/advent2024/Inputs.kt
+++ src/main/kotlin/com/colinodell/advent2024/Inputs.kt
@@ -4,12 +4,11 @@ import java.io.File
 import java.net.URI
 
 internal object Inputs {
-    fun inputAsText(fileName: String): String =
-        File(fileName.toURI()).readText().trimEnd('\n')
+    fun inputAsText(fileName: String): String = File(fileName.toURI()).readText().trimEnd('\n')
 
-    fun inputAsListOfString(fileName: String): List<String> =
-        File(fileName.toURI()).readLines()
+    fun inputAsListOfString(fileName: String): List<String> = File(fileName.toURI()).readLines()
 
     private fun String.toURI(): URI =
-        Inputs.javaClass.classLoader.getResource(this)?.toURI() ?: throw IllegalArgumentException("Cannot find Resource: $this")
+        Inputs.javaClass.classLoader.getResource(this)?.toURI()
+            ?: throw IllegalArgumentException("Cannot find Resource: $this")
 }
