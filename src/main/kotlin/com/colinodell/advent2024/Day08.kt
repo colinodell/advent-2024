41bb0eb9e2ed60aa61e490d5b27c26a99f35b858
Colin O'Dell <codell@seatgeek.com>
2024-12-08 10:45:14 -0500

    Day 8: Resonant Collinearity

---
+++ src/main/kotlin/com/colinodell/advent2024/Day08.kt
@@ -0,0 +1,33 @@
+package com.colinodell.advent2024
+
+class Day08(input: List<String>) {
+    private val grid = input.toGrid()
+    private val region = grid.region()
+    private val antennaPairs = grid
+        // Find all antenna locations
+        .filter { it.value != '.' }
+        .keys
+        // Group by frequency
+        .groupBy { grid[it]!! }
+        // Generate antenna pairs
+        .values
+        .flatMap { it.permutations() }
+
+    fun solvePart1() = antennaPairs
+        .flatMap { (a, b) ->
+            listOf(
+                a - (b - a),
+                b + (b - a),
+            ).filter { it in region }
+        }
+        .toSet()
+        .size
+
+    fun solvePart2() = antennaPairs
+        .flatMap { (a, b) ->
+            generateSequence(a) { it - (b - a) }.takeWhile { it in region } +
+                generateSequence(b) { it + (b - a) }.takeWhile { it in region }
+        }
+        .toSet()
+        .size
+}
--- src/main/kotlin/com/colinodell/advent2024/Extensions.kt
+++ src/main/kotlin/com/colinodell/advent2024/Extensions.kt
@@ -4,3 +4,12 @@ fun Int.clamp(
     min: Int,
     max: Int,
 ) = maxOf(min, minOf(max, this))
+
+fun <T> Collection<T>.permutations() = buildList {
+    val p = this@permutations
+    for (i in p.indices) {
+        for (j in i + 1 until p.size) {
+            add(p.elementAt(i) to p.elementAt(j))
+        }
+    }
+}
